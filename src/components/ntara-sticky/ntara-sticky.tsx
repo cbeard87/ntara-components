315e2b7a0803f6ccff6a1328ec52cf9f46326371
Christopher Beard <beard@ntara.com>
2019-06-24 13:44:29 -0400

    Adds additional functionality for IE11 browsers

--- src/components/ntara-sticky/ntara-sticky.tsx
+++ src/components/ntara-sticky/ntara-sticky.tsx
@@ -18,13 +18,19 @@ export class Sticky {
     return isIE;
   }
 
+  // TODO: Clean this function up
   polyfillSticky() {
     const elPosition = this.el.getBoundingClientRect().top;
+    const elHeight = this.el.getBoundingClientRect().height;
+    const nextEl = this.el.nextElementSibling as HTMLElement;
+    this.el.style.width = '100%';
     document.addEventListener('scroll', () => {
       if (window.pageYOffset >= elPosition - this.offset) {
         this.el.style.position = 'fixed';
+        nextEl.style.marginTop = `${elHeight}px`;
       } else {
         this.el.style.position = 'static';
+        nextEl.style.marginTop = `0px`;
       }
     });
   }
