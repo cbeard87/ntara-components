561e3b33ec15329de07421adc7816de02b3553e7
Christopher Beard <cbeard87@gmail.com>
2019-06-26 00:25:25 -0400

    Optimizing performance

--- src/components/ntara-sticky/ntara-sticky.tsx
+++ src/components/ntara-sticky/ntara-sticky.tsx
@@ -8,37 +8,76 @@ import { Component, Prop, Element, Host, h } from '@stencil/core';
 export class Sticky {
   @Element() el: HTMLElement;
 
+  @Prop() fixedWidth = 'auto';
+  @Prop() defaultPosition = 'static';
   @Prop() offset = 0;
+  @Prop() parent = this.el.parentElement;
+  @Prop() userAgent = window.navigator.userAgent;
   @Prop() zIndex = 1;
 
-  private stickySupported(): boolean {
-    const ua = window.navigator.userAgent;
-    const isIE = !~ua.indexOf('Trident');
-
-    return isIE;
-  }
-
-  // TODO: Clean this function up
-  polyfillSticky() {
-    const elPosition = this.el.getBoundingClientRect().top;
-    const elHeight = this.el.getBoundingClientRect().height;
-    const nextEl = this.el.nextElementSibling as HTMLElement;
-    this.el.style.width = '100%';
-    document.addEventListener('scroll', () => {
-      if (window.pageYOffset >= elPosition - this.offset) {
-        this.el.style.position = 'fixed';
-        nextEl.style.marginTop = `${elHeight}px`;
-      } else {
-        this.el.style.position = 'static';
-        nextEl.style.marginTop = `0px`;
-      }
-    });
-  }
+  stickySupported = !~this.userAgent.indexOf('Trident');
 
   componentDidLoad() {
-    if (!this.stickySupported()) {
+    if (!this.stickySupported) {
       this.polyfillSticky();
     }
+  }
+
+  private polyfillSticky() {
+    let elTop = this.el.getBoundingClientRect().top;
+    let elHeight = this.el.getBoundingClientRect().height;
+    let elWidth = this.el.getBoundingClientRect().width;
+    let lastScrollY = 0;
+    let ticking = false;
+
+    let clone = document.createElement('div');
+    clone.style.cssText = `
+      height: ${elHeight + this.offset}px; width: ${elWidth}px; visibility: hidden; display: none;
+    `;
+    this.parent.insertBefore(clone, this.el);
+
+    // Set default position on component load.
+    this.el.style.position = this.defaultPosition;
+
+    const setFixed = () => {
+      if (lastScrollY >= elTop) {
+        this.el.style.width = this.fixedWidth;
+        this.el.style.position = 'fixed';
+        clone.style.display = 'block';
+      } else {
+        this.el.style.width = 'auto';
+        this.el.style.position = this.defaultPosition;
+        clone.style.display = 'none';
+      }
+    }
+
+    const update = () => {
+      setFixed();
+      ticking = false;
+    }
+
+    const requestTick = () => {
+      if (!ticking) {
+        window.requestAnimationFrame(update);
+        ticking = true;
+      }
+    }
+
+    const onScroll = () => {
+      lastScrollY = window.pageYOffset;
+      requestTick();
+    }
+
+    const onResize = () => {
+      setTimeout(() => {
+        elTop = this.el.getBoundingClientRect().top;
+        lastScrollY = window.pageYOffset;
+        setFixed();
+      }, 500);
+    }
+
+    window.addEventListener('scroll', onScroll);
+    window.addEventListener('resize', onResize);
   }
 
   render() {
